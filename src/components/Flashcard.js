cf6be10223e9a752ff6e4e8bea0c20e50b4489d3
Norman Amorganda <normsamorganda@gmail.com>
2023-02-10 23:19:46 +0800

    uploading flashcard

---
+++ src/components/Flashcard.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef }from 'react'
+import '../App.css'
+const Flashcard = ({flashcard}) => {
+
+  const [flip, setFlip] = useState(false);
+  const  [height, setHeight] = useState('initial')
+  const frontEl = useRef();
+  const backEl = useRef();
+
+  const setMaxHeight = () => {
+    const frontHeight = frontEl.current.getBoundingClientRect().height;
+    const backHeight = backEl.current.getBoundingClientRect().height;
+    setHeight(Math.max(frontHeight, backHeight, 100));
+  }
+
+  useEffect(setMaxHeight,[flashcard.question, flashcard.answer, flashcard.options])
+ useEffect(() => {
+window.addEventListener('resize',setMaxHeight);
+return () => window.removeEventListener('rezie', setMaxHeight)
+ },[])
+
+
+
+
+
+  return (
+    <div 
+    className={`card ${flip ? 'flip' : ''}`}
+    style={{height: height}}
+    onClick={() => {setFlip(!flip)}}>
+
+      <div className='front' ref={frontEl}>
+            {flashcard.question}
+          <div className='flashcard-options'>
+            {flashcard.options.map(option => {
+              return <div className='flashcard-option' key={option}>{option}</div>
+            })}
+          </div>
+
+      </div>
+
+      <div className='back' ref={backEl}> {flashcard.answer}</div>
+
+    {/* {flip ? flashcard.answer : flashcard.question }   */}
+   
+      
+    </div>
+  )
+}
+
+export default Flashcard
